4209a0cab3032e50cad9f6a0af498c25f8d945af
clayton <me@clayton.cc>
2020-02-16 21:09:36 -0800

    Gimme dat song

--- app/routes.php
+++ app/routes.php
@@ -10,3 +10,21 @@ $app->get('/', function (Request $request, Response $response) {
 $app->get('/remove-tapir', function (Request $request, Response $response) {
     return $this->renderer->render($response, 'remove-tapir.phtml');
 });
+
+$app->get('/gimmedatsong', function (Request $request, Response $response) {
+    return $this->renderer->render($response, 'gimmedatsong.phtml');
+});
+
+$app->get('/gimmedatsong/{videoId}', function (Request $request, Response $response, $args) {
+    if (!preg_match('/^[a-z0-9_-]{11}$/i', $args['videoId'])) {
+        return 'invalid video id';
+    }
+
+    $filename = mb_substr(shell_exec('youtube-dl -f bestaudio --get-filename ' . $args['videoId']), 0, -1);
+
+    if (!file_exists('gimmedatsongstorage/' . $filename)) {
+        $output = shell_exec('cd gimmedatsongstorage && youtube-dl -f bestaudio ' . $args['videoId']);
+    }
+
+    return $response->withRedirect('/gimmedatsongstorage/' . rawurlencode($filename), 302);
+});
